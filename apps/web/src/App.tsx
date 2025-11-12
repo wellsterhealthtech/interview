ba07d2b716e117ab8b9e518076ed9ef16adca8e0
Giuseppe Turturiello <giuseppe.turturiello@wellsterhealth.com>
2025-11-12 15:37:44 +0100

    feat(web): add tailwind with custom theming

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,30 +1,28 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./styles/app.css";
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<>
+		<main className="wrapper">
 			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
+				<a className="animated-underline" data-config-color="accent" href="https://vite.dev">
+					Vite
 				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
+				<a className="animated-underline" href="https://react.dev">
+					React
 				</a>
 			</div>
 			<h1>Vite + React</h1>
-			<div className="card">
+			<div>
 				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
 			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-		</>
+			<p>Click on the Vite and React logos to learn more</p>
+		</main>
 	);
 }
 
