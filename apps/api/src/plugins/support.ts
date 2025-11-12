dcda57ed46c720665f1976650ec16c22852c110c
Giuseppe Turturiello <giuseppe.turturiello@wellsterhealth.com>
2025-11-12 14:52:28 +0100

    chore: initial monorepo setup

---
+++ apps/api/src/plugins/support.ts
@@ -0,0 +1,19 @@
+import fp from "fastify-plugin";
+
+export interface SupportPluginOptions {
+	// Specify Support plugin options here
+	message?: string;
+}
+
+// The use of fastify-plugin is required to be able
+// to export the decorators to the outer scope
+export default fp<SupportPluginOptions>(async (fastify, _opts) => {
+	fastify.decorate("someSupport", () => "sending hugs");
+});
+
+// When using .decorate you have to specify added properties for Typescript
+declare module "fastify" {
+	export interface FastifyInstance {
+		someSupport(): string;
+	}
+}
---
+++ apps/api/src/routes/example/index.ts
@@ -0,0 +1,7 @@
+import type { FastifyPluginAsync } from "fastify";
+
+const example: FastifyPluginAsync = async (fastify, _opts): Promise<void> => {
+	fastify.get("/", async (_request, _reply) => "this is an example");
+};
+
+export default example;
